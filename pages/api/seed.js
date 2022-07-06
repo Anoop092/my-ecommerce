4cb00e2978622c509466306012be237145fc0aa7
anoop <anoopvantagodi420@gmail.com>
2022-07-06 22:32:39 +0530

    added products in mongodb

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,11 +1,14 @@
 import db from "../../utils/db";
 import User from "../../models/Users";
 import { data } from "../../utils/products";
+import Product from "../../models/Product";
 
 const Handler = async(req,res)=>{
     await db.connect();
     await User.deleteMany();
     await User.insertMany(data.users);
+    await Product.deleteMany();
+    await Product.insertMany(data.products);
     await db.disconnect();
     res.send({message:'seeded successfully'})
 
